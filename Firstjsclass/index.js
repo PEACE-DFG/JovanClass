d02516d5da6fd09e8d6d200c04d0d54d31c96694
PEACE-DFG <awofesobipeace@gmail.com>
2023-10-21 20:40:08 +0100

    Javascript Inttroduction

---
+++ Firstjsclass/index.js
@@ -0,0 +1,85 @@
+// Single line comment
+
+/*
+ * Multiple line comment
+ */
+
+
+ // OUTPUTING JAVASCRIPT
+
+ //console -> used for testing
+ console.log("Hello World");
+
+ // document.write("Hello World)-> testing process
+
+ document.write("Hello World")
+
+ //window.alert()-> testing and production
+
+ window.alert("Hello World")
+
+ // DOM-> document Object Model
+
+ document.getElementById("demo").innerHTML="this is a DOM approach to outputing javascript"
+
+ //Statements -> Values,operators,expressions,keywords and comments
+
+ //example of keywords are-> var,let,const,if.....
+
+ //SYNTAX-> syntax is a set rules that must be followed to successfully perform the function 
+
+ //to declare variables
+ //1. let
+ //2. var
+ //3. const
+
+ let x;
+ let y;
+var z;
+let X;
+
+
+
+//assigned variables
+x=11;
+X=10;
+y=5;
+z=2;
+let Jovan_114= 'I am learning javascipt'
+
+const w=5;
+
+
+console.log(x+y+z);
+console.log(Jovan_114)
+
+//example of operators are-> +, -, *, /, %
+
+console.log(x%z)
+console.log(x*z)
+console.log(x)
+console.log(X)
+
+
+/*
+VARIABLES:
+
+ARE CONTAINERS FOR STORING DATA
+
+characteristics of variables:
+1. can be declared and initialized
+2. can be reassigned [changed] except for const
+3. Unique names [ their names are called identifiers]
+4. names can contain letters,digits,undercore and dollar sign
+5. names cannot start with a digit
+6. SHOULD NOT INCLUDE SPACE
+7. case sensitive characters
+8. do not use keyword as variables names
+
+
+*/
+
+
+
+
+
