e382627ca502fbb72f6a1f89f51c6542994f6b5d
PEACE-DFG <awofesobipeace@gmail.com>
2023-10-28 20:55:33 +0100

    secondjsclass

---
+++ secondjsclass/index.js
@@ -0,0 +1,93 @@
+// Arithmetic Operators
+
+let x= 100 + 50;
+
+console.log(x);
+console.log(x-20);
+console.log(x*2);
+console.log(x/5);
+console.log(x%12);
+
+console.log(5%2);
+console.log(5**2);
+
+let y=5;
+y--;
+let z=y;
+console.log(z);
+
+/*
+Order / Precendence
+1. () -> brackets
+2. ** -> Exponentials
+3. * and / -> Multiplication and division
+4. Addition and Subtraction -> + and -
+
+*/
+let m= (1+2)*2 -5 +1
+// 3*2 -5 +1
+// 6 -5 +1
+// 1+ 1 
+console.log(m)
+
+let r=4;
+console.log(r);
+
+let Name="Hello Jovan"
+console.log(Name)
+let age =50;
+console.log(age)
+
+let p=2;
+let q=5;
+let l=9;
+
+let f=p==q
+console.log(f);
+console.log(p!=q);
+// Compare the value of p and q
+
+
+// Arrays
+const cars = ["Honda","BMW", "Hyndai"]
+//index numbers= [0 ,     1,      2   ]
+console.log(cars)
+// arrays are called with its index number  and index number starts from zeros
+console.log(cars[2])
+
+let s=0;
+console.log(s)
+
+// Object
+const person= {
+  firstName:"Jovan",
+  lastName:"Javascript",
+  Nickname:'JJ',
+  age:50,
+  eyeColor:'brown'
+
+}
+console.log(person)
+console.log(person.age)
+console.log(person.eyeColor)
+
+// typeof
+
+console.log(typeof(x))
+console.log(typeof(cars))
+console.log(typeof(person))
+console.log(typeof(f))
+console.log(typeof("bola"))
+
+console.log( 2+ 12 + "2")
+
+console.log("2" + 2)
+
+console.log(" jovan" + "Javscript")
+
+let  firstName="Jovan"
+let lastName="Javascript"
+
+document.write(firstName + " " + lastName)
+// Concatenation ->  adding two or more strings together
+
